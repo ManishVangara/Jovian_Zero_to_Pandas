68071649ecea1736a110514558a4b652bd291b38
Manish Vangara <86878532+ManishVangara@users.noreply.github.com>
2023-03-28 21:39:24 +0530

    Changes to test.py file

--- test.py
+++ test.py
@@ -8,3 +8,17 @@ print(arr2)
 
 arr3 = np.arange(1,19)
 print(arr3.reshape(6,-1))
+
+arr4 = np.eye(4)
+
+print("Identity Matrix of shape 4x4", arr4)
+print(arr4.shape)
+
+arr5 = np.array([[[1, 2, 3],
+                 [2, 3, 4]],
+                 [[3, 4, 5],
+                 [4, 5, 6]]])
+
+print(arr5)
+print(arr5.ndim)
+print(arr5.shape)
